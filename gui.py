764a5abc038327356a712520b08fef15aab63f31
Henry Joseph <henry@delyorkgroup.com>
2024-12-07 19:49:37 +0100

    Revert "Create a basic GUI without interactions"
    This reverts commit 773167f3e2955379f4501e2c25372d9967af8aa7.

--- gui.py
+++
@@ -1,9 +0,0 @@
-import functions
-import FreeSimpleGUI as fsg
-
-label = fsg.Text("Enter A Todo:")
-input_box = fsg.InputText(tooltip="Enter a to-do", expand_y=300)
-add_button = fsg.Button("Add")
-window = fsg.Window("My To-do App", layout=[[label], [input_box, add_button]])
-window.read()
-window.close()
